2be15c92ffc2b1ed7193761cccf4a4fb927a83db
Artem Sapegin <artem@sapegin.ru>
2017-06-17 10:52:47 +0200

    Chore: Mocha → Jest

--- test/presets.spec.js
+++ test/presets.spec.js
@@ -1,12 +1,10 @@
-var expect = require('chai').expect;
-
 describe('presets', function() {
 	['index.js', 'legacy.js', 'react.js'].forEach(function(name) {
 		it('should no throw when requring ' + name, function() {
 			var func = function() {
 				require('../' + name);
 			};
-			expect(func).not.to.throw;
+			expect(func).not.toThrow();
 		});
 	});
 });
--- test/rules.spec.js
+++ test/rules.spec.js
@@ -1,14 +1,17 @@
 var fs = require('fs');
 var path = require('path');
-var expect = require('chai').expect;
 
 describe('rules', function() {
 	fs.readdirSync(path.resolve(__dirname, '../rules')).forEach(function(name) {
+		if (!name.match(/\.js$/)) {
+			return;
+		}
+
 		it('should no throw when requring ' + name, function() {
 			var func = function() {
 				require('../rules/' + name);
 			};
-			expect(func).not.to.throw;
+			expect(func).not.toThrow();
 		});
 	});
 });
