2dd8a9841190994eda579ff39844923aca644db4
Artem Sapegin <artem@sapegin.ru>
2023-06-24 11:31:31 +0200

    fix: Remove complexity rule, allow void as a statement

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -1,43 +1,41 @@
 module.exports = {
 	rules: {
 		// Enforces return statements in callbacks of array’s methods
-		'array-callback-return': 2,
+		'array-callback-return': 'error',
 		// Treat var statements as if they were block scoped
-		'block-scoped-var': 2,
-		// Specify the maximum cyclomatic complexity allowed in a program
-		complexity: [0, 11],
+		'block-scoped-var': 'error',
 		// Require return statements to either always or never specify values
-		'consistent-return': 2,
+		'consistent-return': 'error',
 		// Require curly braces for all control statements
-		curly: [2, 'all'],
+		curly: ['error', 'all'],
 		// Encourages use of dot notation whenever possible
 		'dot-notation': [
-			2,
+			'error',
 			{
 				allowKeywords: true,
 			},
 		],
 		// Require the use of === and !==
-		eqeqeq: [2, 'allow-null'],
+		eqeqeq: ['error', 'allow-null'],
 		// Disallow the use of alert, confirm, and prompt
-		'no-alert': 2,
+		'no-alert': 'error',
 		// Disallow using an async function as a Promise executor
-		'no-async-promise-executor': 2,
+		'no-async-promise-executor': 'error',
 		// Disallow use of arguments.caller or arguments.callee
-		'no-caller': 2,
+		'no-caller': 'error',
 		// Disallow else after a return in an if
-		'no-else-return': 0,
+		'no-else-return': 'off',
 		// Disallow use of eval()
-		'no-eval': 2,
+		'no-eval': 'error',
 		// Disallow adding to native types
-		'no-extend-native': 2,
+		'no-extend-native': 'error',
 		// Disallow unnecessary function binding
-		'no-extra-bind': 2,
+		'no-extra-bind': 'error',
 		// Disallow the use of leading or trailing decimal points in numeric literals
-		'no-floating-decimal': 2,
+		'no-floating-decimal': 'error',
 		// Disallow the type conversions with shorter notations
 		'no-implicit-coercion': [
-			2,
+			'error',
 			{
 				boolean: false,
 				number: true,
@@ -45,64 +43,69 @@ module.exports = {
 			},
 		],
 		// Disallow use of eval()-like methods
-		'no-implied-eval': 2,
+		'no-implied-eval': 'error',
 		// Disallow usage of __iterator__ property
-		'no-iterator': 2,
+		'no-iterator': 'error',
 		// Disallow use of labels for anything other then loops and switches
 		'no-labels': [
-			2,
+			'error',
 			{
 				allowLoop: true,
 				allowSwitch: true,
 			},
 		],
 		// Disallow unnecessary nested blocks
-		'no-lone-blocks': 2,
+		'no-lone-blocks': 'error',
 		// Disallow creation of functions within loops
-		'no-loop-func': 2,
+		'no-loop-func': 'error',
 		// Disallow characters which are made with multiple code points in character class syntax
-		'no-misleading-character-class': 2,
+		'no-misleading-character-class': 'error',
 		// Disallow use of multiline strings
-		'no-multi-str': 2,
+		'no-multi-str': 'error',
 		// Disallow use of new operator for Function object
-		'no-new-func': 2,
+		'no-new-func': 'error',
 		// Disallows creating new instances of String, Number, and Boolean
-		'no-new-wrappers': 2,
+		'no-new-wrappers': 'error',
 		// Disallow use of octal escape sequences in string literals, such as var foo = 'Copyright \251';
-		'no-octal-escape': 2,
+		'no-octal-escape': 'error',
 		// Disallow usage of __proto__ property
-		'no-proto': 2,
+		'no-proto': 'error',
 		// Disallow use of assignment in return statement
-		'no-return-assign': 2,
+		'no-return-assign': 'error',
 		// Disallow use of `javascript:` urls.
-		'no-script-url': 2,
+		'no-script-url': 'error',
 		// Disallow comparisons where both sides are exactly the same
-		'no-self-compare': 2,
+		'no-self-compare': 'error',
 		// Disallow use of comma operator
-		'no-sequences': 2,
+		'no-sequences': 'error',
 		// Restrict what can be thrown as an exception
-		'no-throw-literal': 2,
+		'no-throw-literal': 'error',
 		// Disallow unmodified conditions of loops
-		'no-unmodified-loop-condition': 2,
+		'no-unmodified-loop-condition': 'error',
 		// Disallow usage of expressions in statement position
-		'no-unused-expressions': 2,
+		'no-unused-expressions': 'error',
 		// Disallow unnecessary .call() and .apply()
-		'no-useless-call': 2,
+		'no-useless-call': 'error',
 		// Disallow unnecessary catch clauses
-		'no-useless-catch': 2,
+		'no-useless-catch': 'error',
 		// Disallow unnecessary concatenation of literals or template literals
-		'no-useless-concat': 2,
+		'no-useless-concat': 'error',
 		// Disallow redundant return statements
-		'no-useless-return': 2,
+		'no-useless-return': 'error',
 		// Disallow use of void operator
-		'no-void': 2,
+		'no-void': [
+			'error',
+			{
+				allowAsStatement: true,
+			},
+		],
 		// Disallow use of the with statement
-		'no-with': 2,
+		'no-with': 'error',
 		// Disallow async functions which have no await expression
-		'require-await': 2,
+		'require-await': 'error',
 		// Require or disallow Yoda conditions
 		yoda: [
-			2,
+			'error',
 			'never',
 			{
 				exceptRange: true,
