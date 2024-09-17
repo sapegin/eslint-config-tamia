13c33f75093c0a1854630aca35d3b9f393c5fb12
Artem Sapegin <artem@sapegin.ru>
2024-09-17 09:42:05 +0200

    fix: Disable unicorn/no-magic-array-flat-depth rule as too opinionated

--- rules/unicorn.mjs
+++ rules/unicorn.mjs
@@ -4,19 +4,21 @@ export default [
 	eslintPluginUnicorn.configs['flat/recommended'],
 	{
 		rules: {
-			// Prefer reading a JSON file as a buffer
-			'unicorn/prefer-json-parse-buffer': 'error',
-			// Reduces readability
-			'unicorn/prefer-ternary': 'off',
 			// Opinionated, doesn't allow Button.tsx
 			'unicorn/filename-case': 'off',
 			// Opinionated, sometimes you need it
 			'unicorn/no-array-reduce': 'off',
+			// This is too opinionated
+			'unicorn/no-magic-array-flat-depth': 'off',
+			// I don't need this
+			'unicorn/no-process-exit': 'off',
+			// Prefer reading a JSON file as a buffer
+			'unicorn/prefer-json-parse-buffer': 'error',
 			// Set.has() might be faster than Array.includes() but who cares
 			// if the array is short. Set is less readable
 			'unicorn/prefer-set-has': 'off',
-			// I don't need this
-			'unicorn/no-process-exit': 'off',
+			// Reduces readability
+			'unicorn/prefer-ternary': 'off',
 			// Disable abbreviation replacements because it breaks many
 			// established naming conventions and often produces ugly code, like
 			// `import { vars as variables } from '.'`
